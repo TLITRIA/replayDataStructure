393e61bcff1a2609ef0f34e01ec171c6af708bc2
TLITRIA <darkfor9es0t@163.com>
2023-12-25 15:28:04 +0800

    [新增] 完成二叉输的初始化API接口

--- BinarySearchTree/binarySearchTree.c
+++ BinarySearchTree/binarySearchTree.c
@@ -1,11 +1,48 @@
 #include "binarySearchTree.h"
+#include "common.h"
+#include <stdio.h>
+
+enum STATUS_CODE
+{
+    MALLOC_ERR,
+    ON_SUCCESS,
+};
+
+#define JUDGE_MALLOC(p)\
+if (NULL == p)\
+    return MALLOC_ERR;\
 
 /* 二叉搜索树的初始化 */
 int binarySearchTreeInit(BinarySearchTree **pBstree)
 {
-    int ret = 0;
+
+    BinarySearchTree * bstree = (BinarySearchTree *)malloc(sizeof(BinarySearchTree) * 1);
+    JUDGE_MALLOC(bstree);
+    memset(bstree, 0, sizeof(BinarySearchTree) * 1);
+
+
+    {
+        bstree->root = NULL;
+        bstree->size = 0;
+    }
+
+    #if 1
+    /* 分配根节点 */
+    bstree->root = (BSTTreeNode *)malloc(sizeof(BSTTreeNode) * 1);
+    JUDGE_MALLOC(bstree->root);
+    memset(bstree->root, 0, sizeof(BSTTreeNode) * 1);
     
-    return ret;
+    {
+        bstree->root->data = 0;
+        bstree->root->left = NULL;
+        bstree->root->right = NULL;
+        bstree->root->parent = NULL;
+    }
+#endif
+    *pBstree = bstree;
+
+
+    return ON_SUCCESS;
 }
 
 /* 二叉搜索树的插入 */
