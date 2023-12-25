490085bec4a1df3b4a5617da63d2c0e7de91cafa
TLITRIA <darkfor9es0t@163.com>
2023-12-25 16:15:05 +0800

    [新增] 完成二叉搜索树的插入结点API

--- BinarySearchTree/binarySearchTree.c
+++ BinarySearchTree/binarySearchTree.c
@@ -40,16 +40,92 @@ int binarySearchTreeInit(BinarySearchTree **pBstree)
     }
 #endif
     *pBstree = bstree;
-
-
     return ON_SUCCESS;
 }
+
+static int compareFunc(ELEMENTTYPE val1, ELEMENTTYPE val2)
+{
+#if 0
+    if (val1 < val2)
+    {
+        return -1;
+    }
+    else if (val1 > val2)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+#else
+    return val1 - val2;
+#endif
+    
+}
+
 
 /* 二叉搜索树的插入 */
 int binarySearchTreeInsert(BinarySearchTree *pBstree, ELEMENTTYPE val)
 {
-
-    int ret = 0;
+    if (pBstree->size == 0)
+    {
+        (pBstree->size)++;
+        pBstree->root->data = val;
+    }
     
-    return ret;
+    /* travelNode指向根节点 */
+    BSTTreeNode *travelNode = pBstree->root;
+    BSTTreeNode *parentNode = pBstree->root;
+
+    /* todo */
+    /* 确定符号：放在左边还是右边 */
+    int cmp = 0;
+    while (travelNode != NULL)
+    {
+        parentNode = travelNode;
+        // cmp = val - travelNode->data;
+        cmp = compareFunc(val, travelNode->data);
+        /* 插入元素 < 遍历到的结点 */
+        if (cmp < 0)
+        {
+            travelNode = travelNode->left;
+        }
+        else if (cmp > 0)
+        {
+            travelNode = travelNode->right;
+        }
+        else
+        {
+            /* 覆盖或返回 */
+            return ON_SUCCESS;
+        }
+        
+    }
+
+    /* 分配根节点 */
+    BSTTreeNode *newBstNode = (BSTTreeNode *)malloc(sizeof(BSTTreeNode) * 1);
+    JUDGE_MALLOC(newBstNode);
+    memset(newBstNode, 0, sizeof(BSTTreeNode) * 1);
+    {
+        newBstNode->data = 0;
+        newBstNode->left = NULL;
+        newBstNode->right = NULL;
+        newBstNode->parent = NULL;
+    }
+
+    /* 新结点赋值 */
+    newBstNode->data = val;
+
+    if (cmp < 0)
+    {
+        parentNode->left = newBstNode;
+    }
+    else if (cmp > 0)
+    {
+        parentNode->right = newBstNode;
+    }
+
+    (pBstree->size)++;
+    return ON_SUCCESS;
 }
